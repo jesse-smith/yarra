50db4fbabacd6a6beefdc3c5e4b98cc58b472592
Jesse Smith <jesse.smith@stjude.org>
2024-04-10 10:52:37 -0500

    Add license files and notices

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,28 @@
 /// performance, but support gradual optimization of the implementation for
 /// custom containers.
 
+// This project is dual-licensed under the MIT or Apache 2.0 licenses to mirror
+// the licensing of the Rust programming language. You may choose either license
+// to use this software. See the LICENSE-MIT and LICENSE-APACHE files for the
+// full text of each license, and the below copyright notice and disclaimer for
+// the Apache v2.0 license.
+
+// Apache License, Version 2.0 Copyright Notice and Disclaimer
+// 
+// Copyright `2024` `Jesse Smith`
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 mod array;
 mod array_container;
 mod array_element;
